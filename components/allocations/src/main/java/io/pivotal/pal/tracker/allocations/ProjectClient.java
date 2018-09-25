0ebca7e55f80856e47c001a247d0116704c12a2d
Brian Vullo <bvullo@pivotal.io>
2018-09-25 16:42:44 -0700

    configure circuit breaker
    Signed-off-by: Merric de Launey <mdelauney@pivotal.io>

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,18 +1,31 @@
 package io.pivotal.pal.tracker.allocations;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
 
-    private final RestOperations restOperations;
-    private final String registrationServerEndpoint;
+  private final RestOperations restOperations;
+  private final String registrationServerEndpoint;
+  private final ConcurrentMap<Long, ProjectInfo> projects = new ConcurrentHashMap();
 
-    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
-        this.registrationServerEndpoint = registrationServerEndpoint;
-    }
+  public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+    this.restOperations = restOperations;
+    this.registrationServerEndpoint = registrationServerEndpoint;
+  }
 
-    public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-    }
+  @HystrixCommand(fallbackMethod = "getProjectFromCache")
+  public ProjectInfo getProject(long projectId) {
+    projects.put(
+        projectId,
+        restOperations.getForObject(
+            registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class));
+    return projects.get(projectId);
+  }
+
+  public ProjectInfo getProjectFromCache(long projctId) {
+    return projects.get(projctId);
+  }
 }
--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,18 +1,30 @@
 package io.pivotal.pal.tracker.backlog;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import org.springframework.web.client.RestOperations;
 
 public class ProjectClient {
 
-    private final RestOperations restOperations;
-    private final String endpoint;
+  private final RestOperations restOperations;
+  private final String endpoint;
+  private final ConcurrentMap<Long, ProjectInfo> projects = new ConcurrentHashMap();
 
-    public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations = restOperations;
-        this.endpoint = registrationServerEndpoint;
-    }
+  public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+    this.restOperations = restOperations;
+    this.endpoint = registrationServerEndpoint;
+  }
 
-    public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
-    }
+  @HystrixCommand(fallbackMethod = "getProjectFromCache")
+  public ProjectInfo getProject(long projectId) {
+    projects.put(
+        projectId,
+        restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class));
+    return projects.get(projectId);
+  }
+
+  public ProjectInfo getProjectFromCache(long projctId) {
+    return projects.get(projctId);
+  }
 }
